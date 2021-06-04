0377051cd1d72d2041ebed902c80438cf498e8ba
Adroan Covari Heinen <31225010+Adroan@users.noreply.github.com>
2021-06-04 18:28:33 -0300

    Start board dynamic generator

---
+++ Assets/Scripts/Board/SquareBoardGenerator.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareBoardGenerator : MonoBehaviour
+{
+    public GameObject squareTilePrefab;
+    public int mapWigth = 16;
+    public int mapHeight = 16;
+
+
+    void Start()
+    {
+        
+    }
+
+
+}
